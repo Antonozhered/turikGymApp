543494eceecbafc4429b5084693a8b12c419512c
Anton <anton.ozhered@student.howest.be>
2026-06-07 18:47:32 +0200

    changed backend config

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,16 +1,29 @@
-import type { Core } from '@strapi/strapi';
-
-const config: Core.Config.Middlewares = [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
-
-export default config;
+]
