bf04646d06da15d0ef498b7e5b460d3bd08753f4
Anton <anton.ozhered@student.howest.be>
2026-06-08 13:46:33 +0200

    CORS update

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -15,7 +15,11 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+      origin: [
+        'http://127.0.0.1:5500', 
+        'http://localhost:5500',
+        'https://antonozhered.github.io'
+      ],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
